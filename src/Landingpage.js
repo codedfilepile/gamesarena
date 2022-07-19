8d735f7ca1cf785ed3cf95934aa178edd42aeee1
Jatin Sohlot <64283443+codedfilepile@users.noreply.github.com>
2022-07-20 01:05:47 +0530

    Update Landingpage.js

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -2,76 +2,99 @@ import './App.css';
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import { useState, useEffect } from 'react';
+import { FaSort } from "react-icons/fa";
 //import axios from 'axios';
 
 
 export const Landingpage = () => {
 
-    const [searchTerm, setSearchTerm] = useState("");
+   // const [searchTerm, setSearchTerm] = useState("");
     const [gamesList, setGamesList] = useState([]);
+    const [sorted, setIsSorted] = useState(true)
+    const [searchList, setSearchList] = useState([])
 
-    const gamedata = gamesList.filter((game) => { 
-        if(searchTerm === "") {
-            return game;
-        } else if(game.title !== null && game.title !== undefined && game.title.toLowerCase().includes(searchTerm.toLowerCase())){
-            return game;
+    const sortData = () => {
+        console.log(sorted)
+        setIsSorted(!sorted)
+        let filterdata;
+        let data = gamesList
+        if (sorted === true) {
+            filterdata = data.sort((a, b) => a.score - b.score)
         }
-    }).map((game) =>{
-          return(
-              <tr>
-                  <td>{game.title}</td>
-                  <td style={{width:'15vh'}}>{game.score}</td>
-                  <td>{game.genre}</td>
-                  <td>{game.platform}</td>
-                  <td>{game.editors_choice}</td>
-              </tr>
-          )
-      }
-  )
+        else {
+            filterdata = data.sort((a, b) => b.score - a.score)
+        }
+        console.log(filterdata)
+        setGamesList([...filterdata])
+    }
 
-  // Getting data with API
-  useEffect(() => {
-    fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json')
-    .then(response => response.json())
-    .then(list => setGamesList(list));
+    // Getting data with API
+    useEffect(() => {
+        fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json')
+            .then(response => response.json())
+            .then(list => {
+                setGamesList(list);
+                setSearchList(list);
+            });
 
-    //WE CAN ALSO USE AXIOS HERE
-    // axios
-    // .get('https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json')
-    // .then((response) => {
-    //     setGamesList(response.data);
-    // });
+        //WE CAN ALSO USE AXIOS HERE
+        // axios
+        // .get('https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json')
+        // .then((response) => {
+        //     setGamesList(response.data);
+        // });
 
-  });
+    }, []);
+
+    const search = (text) => {
+       // console.log(text)
+        let data = searchList.filter((game) => {
+            if (text === "") {
+                return game;
+            } else if (game.title !== null && game.title !== undefined && game.title.toLowerCase().includes(text.toLowerCase())) {
+                return game;
+            }
+        })
+       // console.log(data)
+        setGamesList([...JSON.parse(JSON.stringify(data))])
+    }
 
 
-  
-return (
-  <div>
-    <br/>
-        
-        <div className='wrapper' style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
-            <input className='search' type="text" placeholder="Search by Name" onChange={(event)=>{
-                setSearchTerm(event.target.value);
-            }}/>
+
+    return (
+        <div>
+            <br />
+
+            <div className='wrapper' style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                <input className='search' type="text" placeholder="Search by Name" onChange={(event) => {
+                    search(event.target.value);
+                }} />
+            </div>
+
+            <div style={{ display: 'flex', flexDirection: 'column', borderTop: '20px solid white', width: '150vh', paddingLeft: '25vh' }}>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th style={{ width: '15vh' }} onClick={() => sortData()}><div style={{display:'flex', flexDirection:'row',justifyContent: 'center' }}>Score   <div style={{color:'red'}}><FaSort/></div></div></th>
+                            <th>Genre</th>
+                            <th>Platform</th>
+                            <th>Editors Choice</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {gamesList.map(game => (
+                            <tr>
+                                <td>{game.title}</td>
+                                <td style={{ width: '25vh' }}>{game.score}</td>
+                                <td>{game.genre}</td>
+                                <td>{game.platform}</td>
+                                <td>{game.editors_choice}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
         </div>
-
-        <div style={{display:'flex', flexDirection:'column', borderTop:'20px solid white', width:'150vh', paddingLeft:'25vh'}}>
-          <Table striped bordered hover>
-                  <thead>
-                      <tr>
-                      <th>Title</th>
-                      <th style={{width:'15vh'}}>Score</th>
-                      <th>Genre</th>
-                      <th>Platform</th>
-                      <th>Editors Choice</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      {gamedata}
-                  </tbody>
-            </Table>
-      </div>
-    </div>
-      );
-  }
+    );
+}
